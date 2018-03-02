7975420ac1589ca41e9bebd57d66f61f8f2b26f8
Victor Bofill <terranvas@gmail.com>
2018-03-02 14:05:05 -0800

    moved some code around to prevent blog articles from being rendered on new.html

--- scripts/article.js
+++ scripts/article.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const articles = [];
+const articles = []; // eslint-disable-line
 
 function Article (rawDataObj) {
     this.author = rawDataObj.author;
@@ -21,11 +21,3 @@ Article.prototype.toHtml = function () {
 
     return fillTemplate(this);
 };
-
-// REVIEW: We can write an arrow function in one line, without the code block, making the code easier to read. With an arrow function, the 'return' is implicit, we can remove it as well.
-// The .sort() method will rearrange the order of elements in the array and return the original array. This method does not make a copy. See the MDN docs for more details.
-rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
-
-rawData.forEach(articleObject => articles.push(new Article(articleObject)));
-
-articles.forEach(article => $('#articles').append(article.toHtml()));
