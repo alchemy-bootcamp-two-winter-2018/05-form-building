b25c710cc06f73be86725b8b5533b9dac7ab8960
Victor Bofill <terranvas@gmail.com>
2018-03-02 14:05:30 -0800

    added some code to resolve some lint confusion

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -1,3 +1,5 @@
+/* global articles Article*/
+
 'use strict';
 
 const articleView = {};
@@ -75,14 +77,20 @@ articleView.setTeasers = () => {
 // COMMENT: Where is this function called? Why?
 // PUT YOUR RESPONSE HERE
 articleView.initIndexPage = () => {
+    // REVIEW: We can write an arrow function in one line, without the code block, making the code easier to read. With an arrow function, the 'return' is implicit, we can remove it as well.
+    // The .sort() method will rearrange the order of elements in the array and return the original array. This method does not make a copy. See the MDN docs for more details.
+    rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
+
+    rawData.forEach(articleObject => articles.push(new Article(articleObject)));
+
+    articles.forEach(article => $('#articles').append(article.toHtml()));
+
     articleView.populateFilters();
     articleView.handleCategoryFilter();
     articleView.handleAuthorFilter();
     articleView.handleMainNav();
     articleView.setTeasers();
 };
-
-
 
 // COMMENT: Where is this function called? Why?
 // PUT YOUR RESPONSE HERE
